41f291e2fa6507783dbc712c648077b461c407a4
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2022-06-30 18:50:02 -0500

    agregando lightbox para cada servicio de la seccion servicios, responsive

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,52 @@ function mostrarSeccion() {
     }
 }
 window.addEventListener('scroll', mostrarSeccion);
+
+/* LIGHT BOX */
+let lbServicios = document.getElementsByClassName('serv-lb');
+
+// para disenio 3d
+let lbDisenio = document.getElementsByClassName('lb-disenio-container');
+let btnDisenioOn = document.getElementById('btn-disenio');
+let btnDisenioOff = document.getElementById('btn-disenio-off');
+
+btnDisenioOn.addEventListener('click',()=>{
+    lbServicios[0].classList.add('show');
+    lbDisenio[0].classList.add('show-disenio');
+});
+
+btnDisenioOff.addEventListener('click',()=>{
+    lbServicios[0].classList.remove('show');
+    lbDisenio[0].classList.remove('show-disenio');
+});
+
+//para impresion 3d
+let lbImpresion = document.getElementsByClassName('lb-impresion-container');
+let btnImpresionOn = document.getElementById('btn-impresion');
+let btnImpresionOff = document.getElementById('btn-impresion-off');
+
+btnImpresionOn.addEventListener('click',()=>{
+    lbServicios[0].classList.add('show');
+    lbImpresion[0].classList.add('show-impresion');
+});
+
+btnImpresionOff.addEventListener('click',()=>{
+    lbServicios[0].classList.remove('show');
+    lbImpresion[0].classList.remove('show-impresion');
+});
+
+
+//para proyectos educativos
+let lbProyectos = document.getElementsByClassName('lb-proyectos-container');
+let btnProyectosOn = document.getElementById('btn-proyectos');
+let btnProyectosOff = document.getElementById('btn-proyectos-off');
+
+btnProyectosOn.addEventListener('click',()=>{
+    lbServicios[0].classList.add('show');
+    lbProyectos[0].classList.add('show-proyectos');
+});
+
+btnProyectosOff.addEventListener('click',()=>{
+    lbServicios[0].classList.remove('show');
+    lbProyectos[0].classList.remove('show-proyectos');
+});
