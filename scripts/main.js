3401760af6dee4dfb5f8dfa0195e3dd4e8826ed5
Fredy-Rodrigo <rod.cond.fredy@gmail.com>
2023-02-25 13:05:45 -0500

    modificacion, cambio de banner, seccion productos, numeros, imagenes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,39 @@
+/* SLIDER */
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.caroussel-list'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.caroussel-indicadores',
+        arrows: {
+            prev: '.caroussel-anterior',
+            next: '.caroussel-siguiente'
+        },
+        responsive: [
+            {
+              // screens greater than >= 775px
+              breakpoint: 320,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: 'auto',
+                slidesToScroll: 1,
+                itemWidth: 200,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+          ]
+    });
+});
+
 
 /* BARRA DE MENU CON SCROLL AL HACER CLICK */
 const deslizar = (idSeccion) => {
